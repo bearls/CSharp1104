c8a01d10d019afd095b256122d91775ab9b0c167
bearls <61299567+bearls@users.noreply.github.com>
2020-02-20 19:00:38 -0600

    Add files via upload

---
+++ Lab3_5_C_Sharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab3_5_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "Slut";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("I know you are, but what am I? Time for Facebook Jail!");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("Try again");
+            }
+       
+                    
+                    
+        }
+    }
+} 
